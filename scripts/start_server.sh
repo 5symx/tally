b001d1be37ec770075ffd5b2f675426402cf3510
5symx <mingxuanyang@yeah.net>
2025-07-16 03:36:27 +0000

    test:compare with profiling for round 1 / 2 / naive

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -10,7 +10,7 @@ echo $TALLY_SERVER_BIN
 if [[ "$1" == "profile" ]]; then
     echo "Launching Tally Server with nsys profile..."
     # nsys profile -o my_profile_report --delay 5 --duration 10 $TALLY_SERVER_BIN
-    nsys profile --capture-range cudaProfilerApi --trace cuda,osrt,nvtx  --force-overwrite true $TALLY_SERVER_BIN
+    nsys profile --capture-range cudaProfilerApi --trace cuda,osrt,nvtx --stats=true --force-overwrite true $TALLY_SERVER_BIN
 else
     echo "Launching Tally Server directly (without nsys profile)..."
     $TALLY_SERVER_BIN
